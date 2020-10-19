f868887aa3928e7ba9eb81549d6dd3433a02dd0a
silence <imsilence@outlook.com>
2020-10-19 10:43:31 +0800

    add v0.0.2
    Signed-off-by: silence <imsilence@outlook.com>

--- math.go
+++ math.go
@@ -3,6 +3,6 @@ package testmath
 import "fmt"
 
 func Add(n1, n2 int) int {
-	fmt.Println("github.com/imsilence/testmath master")
+	fmt.Println("github.com/imsilence/testmath v0.0.2")
 	return n1 + n2
 }
